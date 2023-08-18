1a9079ff07a398bd738c67a771f9e13f73940ef3
akshay77 <ajakshayjain11@gmail.com>
2023-08-18 23:58:33 +0530

    js

---
+++ functions.js
@@ -0,0 +1,25 @@
+// function game(num) {
+//     if (num % 2 == 0) {
+//         return `this is a even number`
+//     } else {
+//         return `this is a odd number`
+//     }
+// }
+
+// for (i = 0; i <= 25; i++){
+//     console.log(i,game(i));
+// }
+
+// let x = console.log("akshay");
+
+// console.log(x);
+
+// function primenumbers(x) {
+//     for (i = 2; i <= x - 1; i++){
+//         if (x % i == 0) {
+//             return "this is not a prime number"
+//         }
+//     }
+//     return " this is a prime numbers"
+// }
+// console.log(primenumbers(32));
