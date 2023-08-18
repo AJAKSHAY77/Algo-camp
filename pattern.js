0f9ebef21daf10d534e0cd04788b368d399295bd
akshay77 <ajakshayjain11@gmail.com>
2023-08-19 00:00:12 +0530

    patterns

--- pattern.js
+++ pattern.js
@@ -1,2 +1,12 @@
-function pattern
-
+function pattern(n) {
+    for (row = 1; row <= n; row += 1) {
+      let star = "";
+      for (col = 1; col <= n; col += 1) {
+        star = star + "*";
+      }
+      console.log(star);
+    }
+}
+pattern(4)
+pattern(5)
+pattern(6)
